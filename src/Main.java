835d250cc74130b75454ed49824d28d59dac8abc
sschafe <sschafe2@emich.edu>
2021-08-22 16:57:18 -0400

    Working on the circle

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		square();
+		//square();
+		circle();
 	}
 
 	
@@ -12,5 +13,18 @@ public class Main {
 			System.out.println("*			  *");
 		}
 		System.out.println("***************************");
+	}
+	
+	public static void circle() {
+		System.out.println("          ***");
+		System.out.println("      *          *");
+		System.out.println("    *              *");
+		System.out.println("   *                *");
+		System.out.println("  *                  *");
+		System.out.println("  *                  *");
+		System.out.println("   *                *");
+		System.out.println("    *              *");
+		System.out.println("      *          *");
+		System.out.println("          ***");
 	}
 }
